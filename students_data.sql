85a92c12cc4ac094a6fbd16b87d6da440e6c7b8e
mansueto <mansuetoonofre@gmail.com>
2024-03-15 10:16:07 -0400

    MY PROGRESS

---
+++ students_data.sql
@@ -0,0 +1,146 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.18 (Ubuntu 12.18-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.18 (Ubuntu 12.18-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: courses; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.courses (
+);
+
+
+ALTER TABLE public.courses OWNER TO freecodecamp;
+
+--
+-- Name: majors; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.majors (
+);
+
+
+ALTER TABLE public.majors OWNER TO freecodecamp;
+
+--
+-- Name: majors_courses; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.majors_courses (
+);
+
+
+ALTER TABLE public.majors_courses OWNER TO freecodecamp;
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.students (
+    student_id integer NOT NULL,
+    first_name character varying(50) NOT NULL,
+    last_name character varying(50) NOT NULL,
+    major_id integer
+);
+
+
+ALTER TABLE public.students OWNER TO freecodecamp;
+
+--
+-- Name: students_student_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.students_student_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.students_student_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: students_student_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.students_student_id_seq OWNED BY public.students.student_id;
+
+
+--
+-- Name: students student_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.students ALTER COLUMN student_id SET DEFAULT nextval('public.students_student_id_seq'::regclass);
+
+
+--
+-- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.courses  FROM stdin;
+\.
+
+
+--
+-- Data for Name: majors; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.majors  FROM stdin;
+\.
+
+
+--
+-- Data for Name: majors_courses; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.majors_courses  FROM stdin;
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+COPY public.students (student_id, first_name, last_name, major_id) FROM stdin;
+\.
+
+
+--
+-- Name: students_student_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.students_student_id_seq', 1, false);
+
+
+--
+-- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
