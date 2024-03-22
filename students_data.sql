7c631f05d422b30533762373179f31d29dd1465b
mansueto <mansuetoonofre@gmail.com>
2024-03-22 08:02:31 -0400

    Save progress: 220.1

--- students_data.sql
+++ students_data.sql
@@ -25,16 +25,43 @@ SET default_table_access_method = heap;
 --
 
 CREATE TABLE public.courses (
+    course_id integer NOT NULL,
+    courses character varying(100) NOT NULL,
+    course character varying(100) NOT NULL
 );
 
 
 ALTER TABLE public.courses OWNER TO freecodecamp;
 
 --
+-- Name: courses_course_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.courses_course_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.courses_course_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: courses_course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.courses_course_id_seq OWNED BY public.courses.course_id;
+
+
+--
 -- Name: majors; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
 CREATE TABLE public.majors (
+    major_id integer NOT NULL,
+    major character varying(50) NOT NULL
 );
 
 
@@ -51,6 +78,28 @@ CREATE TABLE public.majors_courses (
 ALTER TABLE public.majors_courses OWNER TO freecodecamp;
 
 --
+-- Name: majors_major_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.majors_major_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.majors_major_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: majors_major_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.majors_major_id_seq OWNED BY public.majors.major_id;
+
+
+--
 -- Name: students; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
@@ -58,7 +107,8 @@ CREATE TABLE public.students (
     student_id integer NOT NULL,
     first_name character varying(50) NOT NULL,
     last_name character varying(50) NOT NULL,
-    major_id integer
+    major_id integer,
+    gpa numeric(2,1)
 );
 
 
@@ -87,6 +137,20 @@ ALTER SEQUENCE public.students_student_id_seq OWNED BY public.students.student_i
 
 
 --
+-- Name: courses course_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.courses ALTER COLUMN course_id SET DEFAULT nextval('public.courses_course_id_seq'::regclass);
+
+
+--
+-- Name: majors major_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.majors ALTER COLUMN major_id SET DEFAULT nextval('public.majors_major_id_seq'::regclass);
+
+
+--
 -- Name: students student_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
 --
 
@@ -97,7 +161,7 @@ ALTER TABLE ONLY public.students ALTER COLUMN student_id SET DEFAULT nextval('pu
 -- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.courses  FROM stdin;
+COPY public.courses (course_id, courses, course) FROM stdin;
 \.
 
 
@@ -105,7 +169,7 @@ COPY public.courses  FROM stdin;
 -- Data for Name: majors; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.majors  FROM stdin;
+COPY public.majors (major_id, major) FROM stdin;
 \.
 
 
@@ -121,8 +185,22 @@ COPY public.majors_courses  FROM stdin;
 -- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.students (student_id, first_name, last_name, major_id) FROM stdin;
+COPY public.students (student_id, first_name, last_name, major_id, gpa) FROM stdin;
 \.
+
+
+--
+-- Name: courses_course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.courses_course_id_seq', 1, false);
+
+
+--
+-- Name: majors_major_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.majors_major_id_seq', 1, false);
 
 
 --
@@ -133,11 +211,35 @@ SELECT pg_catalog.setval('public.students_student_id_seq', 1, false);
 
 
 --
+-- Name: courses courses_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.courses
+    ADD CONSTRAINT courses_pkey PRIMARY KEY (course_id);
+
+
+--
+-- Name: majors majors_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.majors
+    ADD CONSTRAINT majors_pkey PRIMARY KEY (major_id);
+
+
+--
 -- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
 --
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (student_id);
+
+
+--
+-- Name: students students_major_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.students
+    ADD CONSTRAINT students_major_id_fkey FOREIGN KEY (major_id) REFERENCES public.majors(major_id);
 
 
 --
